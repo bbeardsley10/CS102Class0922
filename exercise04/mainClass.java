35e505f6570c03807e09e81601e650b3233e00c6
Brenton Beardsley <brenton.beardsley@gmail.com>
2022-09-22 14:25:32 -0500

    updated excercise04 file

--- exercise04/mainClass.java
+++ exercise04/mainClass.java
@@ -4,6 +4,22 @@ public class mainClass {
 
     public static void main(String[] args) {
         
-        System.out.println("hello world");
+        char[] myCharacterArray = {'a','%','$','C','g',' ','d','B'};
+            
+        for(int i = 0; i < myCharacterArray.length; i++)
+        {
+            int numericValueOfChar = (int) myCharacterArray[i];
+
+            if(numericValueOfChar >= 97 && numericValueOfChar <= 122)
+            {
+                int upperCaseNumericValueOfChar = numericValueOfChar - 32;
+                char upperCase = (char)upperCaseNumericValueOfChar;
+                System.out.println(upperCase);
+
+            } else {
+                System.out.println(myCharacterArray[i]);
+            }
+        }
+
     }
 }
