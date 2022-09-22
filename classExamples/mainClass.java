8e7774ea23465c2ffd0ce9b1853a41d1e32e2428
Brenton Beardsley <brenton.beardsley@gmail.com>
2022-09-22 13:48:58 -0500

    intial commit

---
+++ classExamples/mainClass.java
@@ -0,0 +1,13 @@
+package classExamples;
+
+public class mainClass {
+    public static void main(String[] args) {
+        
+        char myChar = 'a';
+
+        int numericalValueOfMyChart = (int)myChar;
+        System.out.println(myChar);
+        
+    }
+    
+}
